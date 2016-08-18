43314fee11da55197405a010426f0e64ec1a6d18
Bean He <withhebing@gmail.com>
2016-08-18 14:09:51 +0800

    setup shape layer with pan action

--- HBElasticView/ViewController.swift
+++ HBElasticView/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Vars
+
+    private let minimalHeight: CGFloat = 50.0
+    private let shapeLayer = CAShapeLayer()
+
+    // MARK: -
+
+    override func loadView() {
+        super.loadView()
+
+        shapeLayer.frame = CGRect(x: 0.0, y: 0.0, width: view.bounds.width, height: minimalHeight)
+        shapeLayer.backgroundColor = UIColor(red: 57/255.0, green: 67/255.0, blue: 89/255.0, alpha: 1.0).CGColor
+        view.layer.addSublayer(shapeLayer)
+
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: "panGestureDidMove:"))
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +37,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Methods
+
+    func panGestureDidMove(gesture: UIPanGestureRecognizer) {
+        if gesture.state == .Ended || gesture.state == .Failed || gesture.state == .Cancelled {
+
+        } else {
+            shapeLayer.frame.size.height = minimalHeight + max(gesture.translationInView(view).y, 0)
+        }
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
 
 }
 
