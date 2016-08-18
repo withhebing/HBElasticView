f37ab8d337abd4f2e47d24a50c2c6b5e52f2a7ac
Bean He <withhebing@gmail.com>
2016-08-18 14:36:42 +0800

    UIView extension for presentationLayer of CALayer

---
+++ HBElasticView/HBElasticViewExtension.swift
@@ -0,0 +1,19 @@
+//
+//  HBElasticViewExtension.swift
+//  HBElasticView
+//
+//  Created by Beans on 16/8/18.
+//  Copyright © 2016年 iceWorks. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func dg_center(usePresentationLayerIfPossible: Bool) -> CGPoint {
+        if usePresentationLayerIfPossible, let presentationLayer = layer.presentationLayer() as? CALayer {
+            return presentationLayer.position
+        }
+        return center
+    }
+}
+
